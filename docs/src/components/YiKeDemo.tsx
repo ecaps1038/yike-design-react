e83ac0ecbe1aeb31b88cf4bf353346763bd5ba04
xiefeng <1294699027@qq.com>
2023-08-09 00:45:10 +0800

    feat: docs demo update

--- docs/src/components/YiKeDemo.tsx
+++ docs/src/components/YiKeDemo.tsx
@@ -1,38 +1,23 @@
 import type React from 'react';
-import { readFileSync } from 'fs';
 
 import DemoCode from './DemoCode';
 import Previewer from './Previewer';
 
-let style = '';
-try {
-  // raw loader work error so use fs to read file
-  style = readFileSync(process.env.YIKE_STYLE_PATH!, 'utf-8');
-} catch (error) {
-  console.error(error);
-}
-
 interface YiKeDemoProps {
   lang: string;
-  id: string;
+  path: string;
   code: string;
 }
 
-const YiKeDemo: React.FC<YiKeDemoProps> = async ({ lang, code, id }) => {
-  const matched = /language-(\w+)/.exec(lang || '')?.[1];
-  const language = matched?.replace(/react/, 'jsx') ?? 'javascript';
-
-  const { default: Demo } = await import(`yike/${id}`);
+const YiKeDemo: React.FC<YiKeDemoProps> = async ({ lang, code, path }) => {
+  const { default: Demo } = await import(`@yike-demo/${path}`);
 
   return (
     <div className="mt-3">
-      <Previewer
-        style={style}
-        component={<Demo />}
-      />
+      <Previewer component={<Demo />} />
       <DemoCode
-        lang={language}
         code={code}
+        lang={lang}
       />
     </div>
   );
