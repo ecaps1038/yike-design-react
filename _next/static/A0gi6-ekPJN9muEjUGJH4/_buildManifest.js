ffa9d5f82eb449cd63baf1239baceef9a3ff64dd
rxshan <131181654+rxshan@users.noreply.github.com>
2023-08-08 10:58:45 +0000

    Deploying to gh-pages from @ ecaps1038/yike-design-react@d2d2228e21efd76ac4851864000a32e5c135a137 🚀

---
+++ _next/static/A0gi6-ekPJN9muEjUGJH4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-0d64e3ac108f05b8.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/pages/_app-359df6c28251540f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2888],{2399:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(66701)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[9774,179],function(){return _(2399),_(27157)}),_N_E=n.O()}]);
