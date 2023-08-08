efb619d862a4e957301885fb0827594902c8a2f8
rxshan <renshan.deng@gmail.com>
2023-08-08 12:59:41 +0800

    fix: build docs error

--- docs/src/app/module/layout.tsx
+++ docs/src/app/module/layout.tsx
@@ -1,36 +1,32 @@
-import React from 'react'
-
-import Layout from '@/components/Layout'
-import { LeftAsideItems } from '@/types'
+import type React from 'react';
+import Layout from '@/components/Layout';
+import type { LeftAsideItems } from '@/types';
 
 const ComponentsLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
-
   const items: LeftAsideItems = [
-    { 
-      title: '通用', 
+    {
+      title: '通用',
       children: [
         { text: 'Button 按钮', link: '/module/button' },
         { text: 'Icon 图标', link: '/module/icon' },
         { text: 'Typegraphy 排版', link: '/module/typegraphy' },
-      ] 
+      ],
     },
-    { 
-      title: '布局', 
-      children: [
-        { text: 'Space 间距', link: '/module/space' },
-      ] 
+    {
+      title: '布局',
+      children: [{ text: 'Space 间距', link: '/module/space' }],
     },
-    { 
-      title: '导航', 
+    {
+      title: '导航',
       children: [
         { text: 'Breadcrumb 面包屑', link: '/module/breadcrumb' },
         { text: 'Dropdown 下拉菜单', link: '/module/dropdown' },
         { text: 'Menu 导航菜单', link: '/module/menu' },
         { text: 'Pagination 分页', link: '/module/pagination' },
-      ] 
+      ],
     },
-    { 
-      title: '数据输入', 
+    {
+      title: '数据输入',
       children: [
         { text: 'Checkbox 复选框', link: '/module/checkbox' },
         { text: 'Input 输入框', link: '/module/input' },
@@ -41,24 +37,22 @@ const ComponentsLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
         { text: 'Stepper 步进器', link: '/module/stepper' },
         { text: 'Switch 开关', link: '/module/switch' },
         { text: 'Textarea 文本域', link: '/module/textarea' },
-      ] 
+      ],
     },
-    { 
-      title: '数据展示', 
+    {
+      title: '数据展示',
       children: [
         { text: 'Avatar 头像', link: '/module/avatar' },
         { text: 'Badge 徽标', link: '/module/badge' },
-      ] 
+      ],
     },
-  ]
+  ];
 
   return (
     <Layout items={items}>
-      <div className='max-w-[800px]'>
-        {children}
-      </div>
+      <div className="max-w-[800px]">{children}</div>
     </Layout>
-  )
-}
+  );
+};
 
-export default ComponentsLayout
+export default ComponentsLayout;
