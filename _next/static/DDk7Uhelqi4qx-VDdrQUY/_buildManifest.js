13982c3b4fcfef41218da31ff5952b6d44b313ec
xiefenga <50828665+xiefenga@users.noreply.github.com>
2023-08-10 17:03:45 +0000

    Deploying to gh-pages from @ ecaps1038/yike-design-react@b94967ff1ce5a62ccc2f53cc05e2452b1170112f 🚀

---
+++ _next/static/DDk7Uhelqi4qx-VDdrQUY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d059380e4c133cae.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
