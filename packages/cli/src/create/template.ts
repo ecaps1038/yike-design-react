db3840658a05445e70670f90a3b634528c3ca6c3
xiefeng <1294699027@qq.com>
2023-08-13 00:39:21 +0800

    feat(cli): update create command

--- packages/cli/src/create/template.ts
+++ packages/cli/src/create/template.ts
@@ -12,4 +12,14 @@ export const ComponentIndex = `import xxxx from './xxxx';
 export { xxxx };
 `;
 
-export const IndexExport = `export * from './components/xxxx'`;
+export const IndexExport = `export * from './components/xxxx'
+`;
+
+export const IndexScss = `@import url('./xxxx.scss');
+`;
+
+export const ComponentScss = `/* stylelint-disable-next-line no-empty-source */
+`;
+
+export const IndexStyle = `import './index.scss';
+`;
