30ecc0d03b94d64d745db8d5d90f10594a0c1f68
fgt1t5y <1501034130@qq.com>
2023-08-13 10:49:48 +0800

    feat(Button): new component - Button

--- packages/yike-design/src/components/Button/Button.tsx
+++ packages/yike-design/src/components/Button/Button.tsx
@@ -1,5 +1,57 @@
-const Button = () => {
-  return <div>Button</div>;
+import type React from 'react';
+import type { Size, Type, Shape, Status } from '../../utils/constant';
+import { isFunction } from '../../utils/helper';
+import { createCssScope } from '../../utils/bem';
+import './style/Button.scss';
+
+interface YkButtonProps {
+  disabled?: boolean;
+  type?: Type;
+  size?: Size;
+  shape?: Shape;
+  status?: Status;
+  loading?: boolean;
+  long?: boolean;
+  children: React.ReactNode;
+
+  onClick: React.MouseEventHandler;
+}
+
+const Button: React.FC<YkButtonProps> = props => {
+  const {
+    disabled = false,
+    type = 'primary',
+    size = 'l',
+    shape = 'round',
+    status = 'primary',
+    loading = false,
+    long = false,
+  } = props;
+
+  const bem = createCssScope('button');
+  const classname = bem([type, status, shape, size], {
+    loading: loading,
+    long: long,
+    disabled: disabled,
+  });
+
+  const handleClick = (ev: React.MouseEvent) => {
+    const { onClick } = props;
+
+    if (isFunction(onClick)) {
+      ev.preventDefault();
+      onClick(ev);
+    }
+  };
+
+  return (
+    <button
+      className={classname}
+      onClick={handleClick}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
--- packages/yike-design/src/utils/helper.ts
+++ packages/yike-design/src/utils/helper.ts
@@ -5,3 +5,11 @@ export const isObject = (val: unknown): val is object => {
 export const isString = (val: unknown): val is string => {
   return typeof val === 'string';
 };
+
+export function isFunction(val: any): val is (...args: any[]) => any {
+  return typeof val === 'function';
+}
+
+export const isArray = (val: unknown): val is string[] => {
+  return Array.isArray(val);
+};
