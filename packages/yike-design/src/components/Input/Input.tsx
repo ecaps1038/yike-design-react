d02ac9e420aef613ccc192a4d9aff396714bd80a
Silence-dream <lyqsl@foxmail.com>
2023-09-04 18:03:06 +0800

    docs: 添加部分API设计

--- packages/yike-design/src/components/Input/Input.tsx
+++ packages/yike-design/src/components/Input/Input.tsx
@@ -1,5 +1,26 @@
-const Input = () => {
-  return <div>Input</div>;
-};
+'use client';
+import type React from 'react';
+import classNames from 'classnames';
+interface InputProps {
+  value: string;
+  defaultValue: string;
+  allowClear: boolean;
+  disabled: boolean;
+  readonly: boolean;
+  style: React.CSSProperties;
+  className: string;
+}
+function Input(props: InputProps) {
+  const { value, className, style } = props;
+  return (
+    <>
+      <input
+        style={style}
+        className={classNames(className)}
+        value={value}
+      />
+    </>
+  );
+}
 
 export default Input;
