c5de81066dac21ad1d36fb92ecf2c95648fd093b
xiefenga <50828665+xiefenga@users.noreply.github.com>
2023-10-08 02:31:35 +0000

    Deploying to gh-pages from @ ecaps1038/yike-design-react@58e797d7f861b81bf26db59e844e452b5dd74ccf 🚀

---
+++ _next/static/chunks/pages/_app-7244facb428ab661.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{7772:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(2670)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(7772),_(2224)}),_N_E=n.O()}]);
