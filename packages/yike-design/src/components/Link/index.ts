92d0ac70f3f293279c7f0b0200cdf039494e1d6f
xiefeng <1294699027@qq.com>
2023-10-13 09:58:46 +0800

    feat(link): add link component

---
+++ packages/yike-design/src/components/Link/index.ts
@@ -0,0 +1,5 @@
+import './style/index.scss';
+import Link from './Link';
+import type { LinkProps } from './interface';
+
+export { Link, type LinkProps };
--- packages/yike-design/src/index.ts
+++ packages/yike-design/src/index.ts
@@ -2,3 +2,4 @@ export * from './components/Button';
 export * from './components/Space';
 export * from './components/Typography';
 export * from './components/Anchor';
+export * from './components/Link';
