fd549efb02ddbd6d8ae925af632547474f8057dc
xiefeng <1294699027@qq.com>
2023-10-21 09:38:30 +0800

    chore: update package.json

--- packages/yike-design/src/styles/index.ts
+++ packages/yike-design/src/styles/index.ts
@@ -1,8 +1,6 @@
 import './index.scss';
 
 // import all components style
-// require.context('../components', true, /^\.\/[^_][\w-]+\/style\/index\.tsx?$/);
-
 import '../components/button/style';
 import '../components/anchor/style';
 import '../components/link/style';
